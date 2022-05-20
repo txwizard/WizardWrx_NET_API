d0ea2607e89cb277ccb1a5333105f91ef38245d5
txwizard <txwizard@gmail.com>
2022-05-20 18:08:32 -0500

    Changee the NuGet pack command line to have it infer the list of dependencies from the project configuration file.

--- EmbeddedTextFile/Properties/AssemblyInfo.cs
+++ EmbeddedTextFile/Properties/AssemblyInfo.cs
@@ -45,4 +45,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ( "8.0.209.0" )]
+[assembly: AssemblyVersion ( "8.0.210.0" )]
