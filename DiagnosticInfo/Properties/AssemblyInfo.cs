2fc0ebd8870c671a487c610cd5059b80fac27aa2
David A. Gray <19792705+txwizard@users.noreply.github.com>
2025-10-19 08:16:38 -0500

    Amend location of readme.md in all NuSpec files, so that it is visible to the NuGet Gallery.

--- DiagnosticInfo/Properties/AssemblyInfo.cs
+++ DiagnosticInfo/Properties/AssemblyInfo.cs
@@ -44,5 +44,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion ( "9.0.111.0" )]
-[assembly: AssemblyInformationalVersion ( "9.0.111" )]
+[assembly: AssemblyFileVersion ( "9.0.112.0" )]
+[assembly: AssemblyInformationalVersion ( "9.0.112" )]
